a86071c94311614ed4da36f1b1b8b38b4e7d701d
frois <frois.dev@gmail.com>
2023-09-05 04:31:19 -0300

    add comment table config

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -10,5 +10,7 @@
         /// </summary>
         public long UserId { get; set; }
         public User User { get; set; }
+        public long PostId { get; set; }
+        public Post Post { get; set; }
     }
 }
---
+++ Repositories/TypeConfigurations/CommentConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using Blog.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.API.Repositories.TypeConfigurations
+{
+    public class CommentConfigurations : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable(nameof(Comment));
+
+            builder.Property(comment => comment.Id).UseIdentityColumn();
+            builder.Property(comment => comment.Content).HasColumnType("nvarchar(max)");
+            builder.Property(comment => comment.CreatedAt).HasColumnType("datetime");
+
+            builder.HasOne(comment => comment.Post);
+        }
+    }
+}
